a34a746d8f0c3e1074517d4c13b66be585e25c89
Daniel Rasmussen <31886975+DanielSaaby@users.noreply.github.com>
2019-03-28 09:01:58 +0100

    Lille mistake :)

--- app/src/main/java/com/example/myfriendlist/Model/ListOfFriends.java
+++ app/src/main/java/com/example/myfriendlist/Model/ListOfFriends.java
@@ -21,7 +21,7 @@ public class ListOfFriends {
         friendList.add(new Friend("Søren Vormisto ", "Aakjærsevej 11", 55.46506827982128, 8.707485440852793, 53834542, "jimmi.kk@live.dk", "https://www.facebook.com/jimmi.kristensen.98", "26011992", "" ));
         friendList.add(new Friend("Frederik Tubæk", "Aakjærsevej 11", 55.46506827982128, 8.707485440852793, 53834542, "jimmi.kk@live.dk", "https://www.facebook.com/jimmi.kristensen.98", "26011992", "" ));
         friendList.add(new Friend("Kent Pedersen", "Aakjærsevej 11", 55.46506827982128, 8.707485440852793, 53834542, "jimmi.kk@live.dk", "https://www.facebook.com/jimmi.kristensen.98", "26011992", "" ));
-        friendList.add(new Friend("Daniel Rasmussen", "Aakjærsevej 11", 55.46506827982128, 8.707485440852793, 53834542, "jimmi.kk@live.dk", "https://www.facebook.com/jimmi.kristensen.98", "26011992", "" ));)
+        friendList.add(new Friend("Daniel Rasmussen", "Aakjærsevej 11", 55.46506827982128, 8.707485440852793, 53834542, "jimmi.kk@live.dk", "https://www.facebook.com/jimmi.kristensen.98", "26011992", "" ));
     }
 
     public ArrayList<Friend> getAll()
